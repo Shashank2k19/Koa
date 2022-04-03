d65cc25d95c69b5971011ae8fc7930001dfbb298
shashank shekhar <mr.shekhar008@gmail.com>
2022-04-03 13:15:31 +0530

    initial commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,3 @@
-/**
- * Template Name: Dewi - v4.7.0
- * Template URL: https://bootstrapmade.com/dewi-free-multi-purpose-html-template/
- * Author: BootstrapMade.com
- * License: https://bootstrapmade.com/license/
- */
 (function () {
   "use strict";
 
@@ -300,7 +294,7 @@
   });
 })();
 
-// OWL CAR
+// OWL Carousel
 
 $(".carousel").owlCarousel({
   margin: 20,
@@ -323,3 +317,57 @@ $(".carousel").owlCarousel({
     },
   },
 });
+
+/**
+ *Form Validation
+ */
+
+// Form Blur Event Listeners
+document.getElementById("name").addEventListener("blur", validateName);
+document.getElementById("email").addEventListener("blur", validateEmail);
+document.getElementById("phone").addEventListener("blur", validatePhone);
+document.getElementById("whatsapp").addEventListener("blur", validatePwhatsapp);
+
+function validateName() {
+  const name = document.getElementById("name");
+  const re = /^[a-zA-Z]{3,10}$/;
+
+  if (!re.test(name.value)) {
+    name.classList.add("is-invalid");
+  } else {
+    name.classList.remove("is-invalid");
+  }
+}
+
+function validateEmail() {
+  const email = document.getElementById("email");
+  const re = /^([a-zA-Z0-9_\-\.]+)@([a-zA-Z0-9_\-\.]+)\.([a-zA-Z]{2,5})$/;
+
+  if (!re.test(email.value)) {
+    email.classList.add("is-invalid");
+  } else {
+    email.classList.remove("is-invalid");
+  }
+}
+
+function validatePhone() {
+  const phone = document.getElementById("phone");
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add("is-invalid");
+  } else {
+    phone.classList.remove("is-invalid");
+  }
+}
+
+function validatePwhatsapp() {
+  const phone = document.getElementById("whatsapp");
+  const re = /^\(?\d{3}\)?[-. ]?\d{3}[-. ]?\d{4}$/;
+
+  if (!re.test(phone.value)) {
+    phone.classList.add("is-invalid");
+  } else {
+    phone.classList.remove("is-invalid");
+  }
+}
