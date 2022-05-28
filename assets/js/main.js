bafb925591adf06470d45db608ed149140e118da
shashank shekhar <mr.shekhar008@gmail.com>
2022-05-28 16:33:45 +0530

    Final commit

--- assets/js/main.js
+++ assets/js/main.js
@@ -210,24 +210,6 @@ $(function () {
     selector: ".glightbox",
   });
 
-  /**
-   * Testimonials slider
-   */
-  new Swiper(".testimonials-slider", {
-    speed: 600,
-    loop: true,
-    autoplay: {
-      delay: 5000,
-      disableOnInteraction: false,
-    },
-    slidesPerView: "auto",
-    pagination: {
-      el: ".swiper-pagination",
-      type: "bullets",
-      clickable: true,
-    },
-  });
-
   function getCurrentScroll() {
     return window.pageYOffset || document.documentElement.scrollTop;
   }
